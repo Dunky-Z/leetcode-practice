5a3ae9066a2803a0181cfc2cbfdb25b1b0d05d9a
Dunky-Z <254758318@qq.com>
2023-06-09 21:37:49 +0800

    update

---
+++ 1376.通知所有员工所需的时间.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1376 lang=cpp
+ *
+ * [1376] 通知所有员工所需的时间
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+
+    }
+};
+// @lc code=end
+
---
+++ test.cpp
@@ -0,0 +1,15 @@
+
+mm.ojbkcdn.com_a_202112_r3itp4i535w.jpg       mm.ojbkcdn.com_a_202201_3fnk4lgz51h.jpg         mm.ojbkcdn.com_a_20220208_yyhpuz0qahy.jpg
+mm.ojbkcdn.com_a_202112_r3iup5dah1s.jpg       mm.ojbkcdn.com_a_202201_3fnnjp4vhmv.jpg         mm.ojbkcdn.com_a_20220208_z1lccgisdyw.jpg
+mm.ojbkcdn.com_a_202112_r3iuzovnhn2.jpg       mm.ojbkcdn.com_a_202201_3fnodic5r5i.jpg         mm.ojbkcdn.com_a_20220208_z32vrhpcwiy.jpg
+mm.ojbkcdn.com_a_202112_r3ivg1fdrjc.jpg       mm.ojbkcdn.com_a_202201_3fnpjuxae2q.jpg         mm.ojbkcdn.com_a_20220208_z3vaexeh4xv.jpg
+mm.ojbkcdn.com_a_202112_r3iwe5veqbl.jpg       mm.ojbkcdn.com_a_202201_3fnqjvlvmxv.jpg         mm.ojbkcdn.com_a_20220208_z4wef0nfscx.jpg
+mm.ojbkcdn.com_a_202112_r3ixjangsn0.jpg       mm.ojbkcdn.com_a_202201_3fns0pc1bs0.jpg         mm.ojbkcdn.com_a_20220208_z5cjfsb2ovy.jpg
+mm.ojbkcdn.com_a_202112_r3j01ddzgw5.jpg       mm.ojbkcdn.com_a_202201_3fnsafyw4i0.jpg         mm.ojbkcdn.com_a_20220208_z5sbzspoq2r.jpg
+mm.ojbkcdn.com_a_202112_r3j2i3ilkec.jpg       mm.ojbkcdn.com_a_202201_3fnv2ey0ox2.jpg         mm.ojbkcdn.com_a_20220208_zb2svxn2jma.jpg
+mm.ojbkcdn.com_a_202112_r3j2smdd5mj.jpg       mm.ojbkcdn.com_a_202201_3fnxav1oofy.jpg         mm.ojbkcdn.com_a_20220208_zbrnlv5j1qr.jpg
+mm.ojbkcdn.com_a_202112_r3j3cjclm41.jpg       mm.ojbkcdn.com_a_202201_3fnxycffrhv.jpg         mm.ojbkcdn.com_a_20220208_zbvpthejnp4.jpg
+mm.ojbkcdn.com_a_202112_r3j4dszvqg0.jpg       mm.ojbkcdn.com_a_202201_3fnyrkw25ic.jpg         mm.ojbkcdn.com_a_20220208_zc3sthx2hwq.jpg
+
+写一段shell脚本，将这些图片按照时间创建文件夹并分类。为保证数据安全，将所有图片都移动到/picture/backup中，文件以软链接形式分类到每个文件夹。图片名称都以mm.ojbkcdn.com_a_开头，之后4个字符表示年份，接着2个字符表示月份。建立年份文件夹并在其中建立月份文件夹。
+如mm.ojbkcdn.com_a_202112_r3j4dszvqg0.jpg放入到文件夹/picture/2021/12/。
